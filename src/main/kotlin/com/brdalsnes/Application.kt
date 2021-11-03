28dca3e587178896dcfa3e780bf32fc917925bd5
Bjørnar Dalsnes <bjornar.dalsnes@knowit.no>
2021-11-03 17:18:37 +0100

    Initial commit

---
+++ src/main/kotlin/com/brdalsnes/Application.kt
@@ -0,0 +1,12 @@
+package com.brdalsnes
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.brdalsnes.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
